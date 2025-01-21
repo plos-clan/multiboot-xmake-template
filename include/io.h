8bd87b5202d20b41831037d8f24a07b6603f0956
XIAOYI12 <100394666+xiaoyi1212@users.noreply.github.com>
2025-01-22 01:50:31 +0800

    补充注释

--- include/io.h
+++ include/io.h
@@ -3,17 +3,17 @@
 #include "klibc.h"
 
 void io_hlt(void);
-void io_cli(void);
-void io_sti(void);
-void io_stihlt(void);
-uint8_t io_in8(uint16_t port);
+void io_cli(void);  //关闭中断
+void io_sti(void);  //打开中断
+void io_stihlt(void);  //阻塞,用于 KernelPanic
+uint8_t io_in8(uint16_t port); // I/O 端口操作封装, in: 输入 | out: 输出
 uint16_t io_in16(uint16_t port);
 uint32_t io_in32(uint16_t port);
 void io_out8(uint16_t port, uint8_t value);
 void io_out16(uint16_t port, uint16_t value);
 void io_out32(uint16_t port, uint32_t value);
 
-void io_hlt(void) {
+void io_hlt(void) { 
     __asm__ __volatile__("hlt");
 }
 
